8f5e84939ffd71039bfa2a9895bd03619db68a29
lil-koruko <54000270+lil-koruko@users.noreply.github.com>
2020-11-09 10:57:26 +0100

    initial commit

---
+++ android/src/main/java/com/whuups/mobile/app/UnlockScreenActivity.java
@@ -0,0 +1,259 @@
+package com.whuups.mobile.app;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+import android.os.Vibrator;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+import android.app.Activity;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.view.CameraView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import com.squareup.picasso.Picasso;
+import com.whuups.mobile.chat.R;
+
+public class UnlockScreenActivity extends AppCompatActivity implements UnlockScreenActivityInterface {
+
+    private static final String TAG = "MessagingService";
+    private TextView tvName;
+    private TextView tvInfo;
+    private ImageView ivAvatar;
+    private String uuid = "";
+    private String call_type = "";
+    static boolean active = false;
+    private static Vibrator v = (Vibrator) IncomingCallModule.reactContext.getSystemService(Context.VIBRATOR_SERVICE);
+    private long[] pattern = {0, 1000, 800};
+    private static MediaPlayer player = MediaPlayer.create(IncomingCallModule.reactContext, Settings.System.DEFAULT_RINGTONE_URI);
+    private static Activity fa;
+
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
+    private int REQUEST_CODE_PERMISSIONS = 1001;
+    private CameraView mCameraView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        active = true;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        active = false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        fa = this;
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null) {
+            if (bundle.containsKey("call_type")) {
+                call_type = bundle.getString("call_type");
+                if (call_type.equals("voice_call")) {
+                    setContentView(R.layout.activity_call_incoming);
+                } else {
+                    setContentView(R.layout.activity_video_call_incoming);
+                    if (allPermissionsGranted()) {
+                        startCamera();
+                    } else {
+                        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+                                REQUEST_CODE_PERMISSIONS);
+                    }
+                }
+            }
+        }
+
+        tvName = findViewById(R.id.tvName);
+        tvInfo = findViewById(R.id.tvInfo);
+        ivAvatar = findViewById(R.id.ivAvatar);
+
+        if (bundle != null) {
+            if (bundle.containsKey("uuid")) {
+                uuid = bundle.getString("uuid");
+            }
+            if (bundle.containsKey("name")) {
+                String name = bundle.getString("name");
+                tvName.setText(name);
+            }
+            if (bundle.containsKey("info")) {
+                String info = bundle.getString("info");
+                tvInfo.setText(info);
+            }
+            if (bundle.containsKey("avatar")) {
+                String avatar = bundle.getString("avatar");
+                if (avatar != null) {
+                    Picasso.get().load(avatar).transform(new CircleTransform()).into(ivAvatar);
+                }
+            }
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+
+        v.vibrate(pattern, 0);
+        player.start();
+
+        AnimateImage acceptCallBtn = findViewById(R.id.ivAcceptCall);
+        acceptCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    v.cancel();
+                    player.stop();
+                    acceptDialing();
+                } catch (Exception e) {
+                    WritableMap params = Arguments.createMap();
+                    params.putString("message", e.getMessage());
+                    sendEvent("error", params);
+                    dismissDialing();
+                }
+            }
+        });
+
+        AnimateImage rejectCallBtn = findViewById(R.id.ivDeclineCall);
+        rejectCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                v.cancel();
+                player.stop();
+                dismissDialing();
+            }
+        });
+
+    }
+
+    public boolean allPermissionsGranted(){
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera();
+            } else{
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private void startCamera() {
+        mCameraView = findViewById(R.id.view_finder);
+        mCameraView.setFlash(ImageCapture.FLASH_MODE_AUTO);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        mCameraView.bindToLifecycle((LifecycleOwner) UnlockScreenActivity.this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Dont back
+    }
+
+    public static void dismissIncoming() {
+        v.cancel();
+        player.stop();
+        fa.finish();
+    }
+
+    private void acceptDialing() {
+        WritableMap params = Arguments.createMap();
+        params.putBoolean("accept", true);
+        params.putString("uuid", uuid);
+        if (!IncomingCallModule.reactContext.hasCurrentActivity()) {
+            params.putBoolean("isHeadless", true);
+        }
+
+        sendEvent("answerCall", params);
+
+        finish();
+    }
+
+    private void dismissDialing() {
+        WritableMap params = Arguments.createMap();
+        params.putBoolean("accept", false);
+        params.putString("uuid", uuid);
+        if (!IncomingCallModule.reactContext.hasCurrentActivity()) {
+            params.putBoolean("isHeadless", true);
+        }
+
+        sendEvent("endCall", params);
+
+        finish();
+    }
+
+    @Override
+    public void onConnected() {
+        Log.d(TAG, "onConnected: ");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.d(TAG, "onDisconnected: ");
+
+    }
+
+    @Override
+    public void onConnectFailure() {
+        Log.d(TAG, "onConnectFailure: ");
+
+    }
+
+    @Override
+    public void onIncoming(ReadableMap params) {
+        Log.d(TAG, "onIncoming: ");
+    }
+
+    private void sendEvent(String eventName, WritableMap params) {
+        IncomingCallModule.reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+}
